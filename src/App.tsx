13cbdfac92cacba7ab217f4924fa1a9ccc636620
MilanPaunovic <50558440+babyrage97@users.noreply.github.com>
2025-08-25 00:08:36 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Layout from "./components/Layout"; // Dodaj Layout
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
-// Import samo postojeće komponente
 import PrivacyPolicy from "./components/legal/PrivacyPolicy";
 import TermsOfService from "./components/legal/TermsOfService";
 import CookiePolicy from "./components/legal/CookiePolicy";
-
 
 const queryClient = new QueryClient();
 
@@ -23,14 +21,15 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
+            {/* Home bez Layout-a */}
             <Route path="/" element={<Index />} />
             
-            {/* Samo postojeće rute */}
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-of-service" element={<TermsOfService />} />
-<Route path="/cookie-policy" element={<CookiePolicy />} />
+            {/* Legal stranice sa Layout-om */}
+            <Route path="/privacy-policy" element={<Layout><PrivacyPolicy /></Layout>} />
+            <Route path="/terms-of-service" element={<Layout><TermsOfService /></Layout>} />
+            <Route path="/cookie-policy" element={<Layout><CookiePolicy /></Layout>} />
 
-            
+            {/* 404 bez Layout-a da bude clean */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
