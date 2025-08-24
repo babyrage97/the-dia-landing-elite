c6ceb9c3e94cf379b2987d9cd3d1a30a699b03f4
MilanPaunovic <50558440+babyrage97@users.noreply.github.com>
2025-08-24 22:37:38 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+
 // Import legal components
-// Umesto inline komponente, koristi:
 import PrivacyPolicy from "./components/legal/PrivacyPolicy";
 import TermsOfService from "./components/legal/TermsOfService";
 import CookiePolicy from "./components/legal/CookiePolicy";
