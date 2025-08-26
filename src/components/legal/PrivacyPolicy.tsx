ad0132ea9270063a585f63ad407f421a48106b4b
MilanPaunovic <50558440+babyrage97@users.noreply.github.com>
2025-08-26 23:40:31 +0200

    Update PrivacyPolicy.tsx

--- src/components/legal/PrivacyPolicy.tsx
+++ src/components/legal/PrivacyPolicy.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navigation from '../ui/Navigation';
-import Footer from '../ui/Footer';
+import Navigation from '../../components/Navigation';
+import Footer from '../../components/Footer';
 
 const PrivacyPolicy = () => {
   return (
